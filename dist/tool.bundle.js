380688851e6db7d13cc71413ff617d5ef219b285
ORNG <acodrst@mail.com>
2024-12-09 14:38:04 -0800

    fpng

--- dist/tool.bundle.js
+++ dist/tool.bundle.js
@@ -1,3 +1,73 @@
+/*
+MIT License
+
+Copyright (c) 2020 Egor Nepomnyaschih
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+/*
+// This constant can also be computed with the following algorithm:
+const base64abc = [],
+    A = "A".charCodeAt(0),
+    a = "a".charCodeAt(0),
+    n = "0".charCodeAt(0);
+for (let i = 0; i < 26; ++i) {
+    base64abc.push(String.fromCharCode(A + i));
+}
+for (let i = 0; i < 26; ++i) {
+    base64abc.push(String.fromCharCode(a + i));
+}
+for (let i = 0; i < 10; ++i) {
+    base64abc.push(String.fromCharCode(n + i));
+}
+base64abc.push("+");
+base64abc.push("/");
+*/
+const base64abc = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
+];
+function bytesToBase64(bytes) {
+    let result = '', i, l = bytes.length;
+    for (i = 2; i < l; i += 3) {
+        result += base64abc[bytes[i - 2] >> 2];
+        result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+        result += base64abc[((bytes[i - 1] & 0x0F) << 2) | (bytes[i] >> 6)];
+        result += base64abc[bytes[i] & 0x3F];
+    }
+    if (i === l + 1) { // 1 octet yet to write
+        result += base64abc[bytes[i - 2] >> 2];
+        result += base64abc[(bytes[i - 2] & 0x03) << 4];
+        result += "==";
+    }
+    if (i === l) { // 2 octets yet to write
+        result += base64abc[bytes[i - 2] >> 2];
+        result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+        result += base64abc[(bytes[i - 1] & 0x0F) << 2];
+        result += "=";
+    }
+    return result;
+}
+
 /*! pako 2.1.0 https://github.com/nodeca/pako @license (MIT AND Zlib) */
 // (C) 1995-2013 Jean-loup Gailly and Mark Adler
 // (C) 2014-2017 Vitaly Puzrin and Andrey Tupitsin
@@ -7959,74 +8029,137 @@ Pxxl.Font.ParseJSON = function (obj) {
 
 })();
 
-/*
-MIT License
-
-Copyright (c) 2020 Egor Nepomnyaschih
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-/*
-// This constant can also be computed with the following algorithm:
-const base64abc = [],
-    A = "A".charCodeAt(0),
-    a = "a".charCodeAt(0),
-    n = "0".charCodeAt(0);
-for (let i = 0; i < 26; ++i) {
-    base64abc.push(String.fromCharCode(A + i));
+function fc(c, i, j) {
+  const dc = [128, 128, 128, 255];
+  dc[Math.floor(Math.random() * (3))] = 255;
+  const colors = [
+    [255, 255, 255, 255],
+    dc,
+    [0, 153, 136, 255],
+    [187, 187, 187, 255],
+  ];
+  for (let k = 0; k < 4; k++) {
+    logo[i * 4 + lwidth * 4 * j + k] = colors[c][k];
+  }
 }
-for (let i = 0; i < 26; ++i) {
-    base64abc.push(String.fromCharCode(a + i));
+const lwidth = 120;
+const factor = .06;
+const lstart = lwidth * 272;
+const logo = new Uint8Array(lwidth * 4 * lwidth).fill(255);
+const llines = ["     FLOPPY", "      PNG"];
+const bl = [0, 0, 0, 255];
+const tl = [0, 153, 136, 255];
+const cy = [51, 187, 238, 255];
+const or = [238, 119, 51, 255];
+const mg = [238, 51, 119, 255];
+
+const pf = Pxxl.Font.ParseBDF(Deno.readTextFileSync("assets/8x16.bdf"));
+for (let i = 0; i < lwidth; i++) {
+  for (let j = 0; j < lwidth; j++) {
+    if (i < 17 * lwidth * factor) fc(2, i, j);
+    if (
+      4 * lwidth * factor < i && i < 13 * lwidth * factor && 0 <= j &&
+      j < 6 * lwidth * factor
+    ) fc(3, i, j);
+    if (
+      3 * lwidth * factor < i && i < 14 * lwidth * factor &&
+      7 * lwidth * factor < j && j < 16 * lwidth * factor
+    ) fc(1, i, j);
+    if (
+      i < 11 * lwidth * factor && i > 9 * lwidth * factor &&
+      j < 5 * lwidth * factor && j > 8
+    ) fc(2, i, j);
+    if (
+      5.2 * lwidth * factor < i && i < 12.2 * lwidth * factor &&
+      9.2 * lwidth * factor < j && j < 13.8 * lwidth * factor
+    ) fc(0, i, j);
+  }
 }
-for (let i = 0; i < 10; ++i) {
-    base64abc.push(String.fromCharCode(n + i));
+for (const line in llines) {
+  const offset = line == 0 ? 0 : 16;
+  const pixels = pf.getPixels(llines[line]);
+  for (const pixel of pixels) {
+    for (let i = 0; i < 4; i++) {
+      logo[
+        offset + lstart - 4 + lwidth * 4 * 16 * line +
+        (pixel.x + pixel.y * lwidth) * 4 + i
+      ] = bl[i];
+    }
+  }
 }
-base64abc.push("+");
-base64abc.push("/");
-*/
-const base64abc = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
-];
-function bytesToBase64(bytes) {
-    let result = '', i, l = bytes.length;
-    for (i = 2; i < l; i += 3) {
-        result += base64abc[bytes[i - 2] >> 2];
-        result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
-        result += base64abc[((bytes[i - 1] & 0x0F) << 2) | (bytes[i] >> 6)];
-        result += base64abc[bytes[i] & 0x3F];
+function fpng(text, domain, tss) {
+  const lines = [
+    `${" ".repeat((120 - domain.length) / 2)}${domain.toUpperCase()} ${tss}`,
+    "This image includes website knowledge as triples. Normally, this file is named with <UTC timestamp>-<Adler32>.png. Use OpenSSL to verify the RSA-PSS sig-",
+    "nature using the same name with a .txt extension. The Adler32 checksum is 20 bytes in from the end of the PNG, and can be verified with a binary editor.",
+    " ".repeat(37) +
+    "-> If possible, verify the image at floppypng.com before loading. <-",
+    `The triples and code to render can be extracted using Deno. Run: Deno.writeFileSync("u.z",Deno.readFileSync("<UTC timestamp>-<Adler32>.png").slice(41,-16))`,
+    `Extract with pigz. The extracted data, u, has a null (0x00) at the beginning of each line of 5465 characters. These can be removed automatically with this:`,
+    `let i,n=5465,x='',b=Deno.readFileSync("u");for(i=n*130;i<b.length;i+=n){x+=new TextDecoder().decode(b.slice(i+1,i+n))};Deno.writeTextFile("u.js",x.trim())`,
+  ];
+  const f_sites = 2;
+  const f_tracks = 80;
+  const f_sectors = 18;
+  const f_bytes = 512;
+  const shift = 496;
+  const dat = new TextEncoder().encode(text);
+  const num_lines = 7;
+  const width = 5464;
+  const font_height = 16;
+  const line_below = 1;
+  const top_padding = 6;
+  const bottom_padding = 5;
+  const head_length = width *
+    (num_lines * (font_height + line_below) + top_padding + bottom_padding);
+  console.log(
+    `1.44 MB Floppy Disk Size=${f_sites * f_tracks * f_sectors * f_bytes}`,
+  );
+
+  const head = new Uint8Array(head_length).fill(255);
+  const dat_rows = (dat.length) % width == 0
+    ? (dat.length) / width
+    : Math.floor(dat.length / width) + 1;
+  const dat_fill = new Uint8Array(dat_rows * width - dat.length).fill(
+    32,
+  );
+  const flp_dat_arr = new Uint8Array([
+    ...head,
+    ...dat,
+    ...dat_fill,
+  ]);
+  const start = width * top_padding;
+  for (const line in lines) {
+    const c = line == 0 ? or : line > 0 && line < 3 ? tl : line == 3 ? mg : cy;
+    const pixels = pf.getPixels(lines[line]);
+    for (const pixel of pixels) {
+      for (let i = 0; i < 4; i++) {
+        flp_dat_arr[
+          start + shift + line * (font_height + line_below) * width +
+          (pixel.x + pixel.y * width / 4) * 4 + i
+        ] = c[i];
+      }
     }
-    if (i === l + 1) { // 1 octet yet to write
-        result += base64abc[bytes[i - 2] >> 2];
-        result += base64abc[(bytes[i - 2] & 0x03) << 4];
-        result += "==";
+  }
+  let ptr = 0;
+  let y = 0;
+  for (let x = start; x < start + width * 119; x++) {
+    if (x % width == 0) {
+      ptr = 0;
+      y++;
     }
-    if (i === l) { // 2 octets yet to write
-        result += base64abc[bytes[i - 2] >> 2];
-        result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
-        result += base64abc[(bytes[i - 1] & 0x0F) << 2];
-        result += "=";
-    }
-    return result;
+    if (ptr < 120 * 4) flp_dat_arr[x] = logo[y * 120 * 4 + ptr];
+    ptr++;
+  }
+  return new Uint8Array(
+    UPNG.encode(
+      [flp_dat_arr],
+      width / 4,
+      dat_rows + top_padding + bottom_padding +
+        (font_height + line_below) * num_lines,
+      0,
+    ),
+  );
 }
 
 const emoji = "🍊";
@@ -8034,16 +8167,6 @@ const domain = "orng.org";
 const backup = "CL_ORN_BACKUP";
 const dt = new Date();
 const tss = dt.toISOString().replaceAll(":", "").replaceAll("-", "").replaceAll(".","",);
-let lines = [
-  `${" ".repeat((120 - domain.length) / 2)}${domain.toUpperCase()} ${tss}`,
-  "This image includes website knowledge as triples. Normally, this file is named with <UTC timestamp>-<Adler32>.png. Use OpenSSL to verify the RSA-PSS sig-",
-  "nature using the same name with a .txt extension. The Adler32 checksum is 20 bytes in from the end of the PNG, and can be verified with a binary editor.",
-  " ".repeat(37) +
-  "-> If possible, verify the image at floppypng.com before loading. <-",
-  `The triples and code to render can be extracted using Deno. Run: Deno.writeFileSync("u.z",Deno.readFileSync("<UTC timestamp>-<Adler32>.png").slice(41,-16))`,
-  `Extract with pigz. The extracted data, u, has a null (0x00) at the beginning of each line of 5465 characters. These can be removed automatically with this:`,
-  `let i,n=5465,x='',b=Deno.readFileSync("u");for(i=n*130;i<b.length;i+=n){x+=new TextDecoder().decode(b.slice(i+1,i+n))};Deno.writeTextFile("u.js",x.trim())`,
-];
 //let px = await import("./pxxl.min.js")
 //import "./pako.min.js"
 //import { UPNG } from "./upng.min.js"
@@ -8073,125 +8196,8 @@ const cur_hash=arr_to_hex(new Uint8Array(await crypto.subtle.digest("SHA-512", n
 
 if (last_hash.trim() != cur_hash.trim()){
  Deno.writeTextFileSync('data_sha512.txt',cur_hash);
+  const img=fpng(text,domain,tss);
 
-const pf = Pxxl.Font.ParseBDF(Deno.readTextFileSync("assets/8x16.bdf"));
-const f_sites = 2;
-const f_tracks = 80;
-const f_sectors = 18;
-const f_bytes = 512;
-const bl = [0, 0, 0, 255];
-const tl = [0, 153, 136, 255];
-const cy = [51, 187, 238, 255];
-const or = [238, 119, 51, 255];
-const mg = [238, 51, 119, 255];
-const shift = 496;
-const dat = new TextEncoder().encode(text);
-const num_lines = 7;
-const width = 5464;
-const font_height = 16;
-const line_below = 1;
-const top_padding = 6;
-const bottom_padding = 5;
-const head_length = width *
-  (num_lines * (font_height + line_below) + top_padding + bottom_padding);
-console.log(
-  `1.44 MB Floppy Disk Size=${f_sites * f_tracks * f_sectors * f_bytes}`,
-);
-const lwidth = 120;
-const factor = .06;
-const lstart = lwidth * 272;
-let logo = new Uint8Array(lwidth * 4 * lwidth).fill(255);
-let llines = ["     FLOPPY", "      PNG"];
-function fc(c, i, j) {
-  let dc = [128, 128, 128, 255];
-  dc[Math.floor(Math.random() * (3))] = 255;
-  let colors = [
-    [255, 255, 255, 255],
-    dc,
-    [0, 153, 136, 255],
-    [187, 187, 187, 255],
-  ];
-  for (let k = 0; k < 4; k++) {
-    logo[i * 4 + lwidth * 4 * j + k] = colors[c][k];
-  }
-}
-for (let i = 0; i < lwidth; i++) {
-  for (let j = 0; j < lwidth; j++) {
-    if (i < 17 * lwidth * factor) fc(2, i, j);
-    if (
-      4 * lwidth * factor < i && i < 13 * lwidth * factor && 0 <= j &&
-      j < 6 * lwidth * factor
-    ) fc(3, i, j);
-    if (
-      3 * lwidth * factor < i && i < 14 * lwidth * factor &&
-      7 * lwidth * factor < j && j < 16 * lwidth * factor
-    ) fc(1, i, j);
-    if (
-      i < 11 * lwidth * factor && i > 9 * lwidth * factor &&
-      j < 5 * lwidth * factor && j > 8
-    ) fc(2, i, j);
-    if (
-      5.2 * lwidth * factor < i && i < 12.2 * lwidth * factor &&
-      9.2 * lwidth * factor < j && j < 13.8 * lwidth * factor
-    ) fc(0, i, j);
-  }
-}
-for (let line in llines) {
-  const offset = line == 0 ? 0 : 16;
-  let pixels = pf.getPixels(llines[line]);
-  for (let pixel of pixels) {
-    for (let i = 0; i < 4; i++) {
-      logo[
-        offset + lstart - 4 + lwidth * 4 * 16 * line +
-        (pixel.x + pixel.y * lwidth) * 4 + i
-      ] = bl[i];
-    }
-  }
-}
-const head = new Uint8Array(head_length).fill(255);
-const dat_rows = (dat.length) % width == 0
-  ? (dat.length) / width
-  : Math.floor(dat.length / width) + 1;
-const dat_fill = new Uint8Array(dat_rows * width - dat.length).fill(
-  32,
-);
-const flp_dat_arr = new Uint8Array([
-  ...head,
-  ...dat,
-  ...dat_fill,
-]);
-const start = width * top_padding;
-for (let line in lines) {
-  const c = line == 0 ? or : line > 0 && line < 3 ? tl : line == 3 ? mg : cy;
-  let pixels = pf.getPixels(lines[line]);
-  for (let pixel of pixels) {
-    for (let i = 0; i < 4; i++) {
-      flp_dat_arr[
-        start + shift + line * (font_height + line_below) * width +
-        (pixel.x + pixel.y * width / 4) * 4 + i
-      ] = c[i];
-    }
-  }
-}
-let ptr = 0;
-let y = 0;
-for (let x = start; x < start + width * 119; x++) {
-  if (x % width == 0) {
-    ptr = 0;
-    y++;
-  }
-  if (ptr < 120 * 4) flp_dat_arr[x] = logo[y * 120 * 4 + ptr];
-  ptr++;
-}
-let img = new Uint8Array(
-  UPNG.encode(
-    [flp_dat_arr],
-    width / 4,
-    dat_rows + top_padding + bottom_padding +
-      (font_height + line_below) * num_lines,
-    0,
-  ),
-);
 let a32h = arr_to_hex(img.slice(-20, -16));
 console.log(`Generated FloppyPNG Size=${img.length}`);
 
@@ -8243,12 +8249,6 @@ Deno.writeTextFileSync(
   `${domain}.page.html`,
   page
     .replaceAll("thisisimage", `${tss}-${a32h}`)
-    .replaceAll(
-      "thisisdatstart",
-      (top_padding + bottom_padding + (font_height + line_below) * num_lines) *
-        (width + 1),
-    )
-    .replaceAll("unfilteredlinewidth", width + 1)
     .replaceAll("thisisemoji", emoji),
 );
 }
--- src/tool.js
+++ src/tool.js
@@ -13,9 +13,8 @@ let lines = [
   `Extract with pigz. The extracted data, u, has a null (0x00) at the beginning of each line of 5465 characters. These can be removed automatically with this:`,
   `let i,n=5465,x='',b=Deno.readFileSync("u");for(i=n*130;i<b.length;i+=n){x+=new TextDecoder().decode(b.slice(i+1,i+n))};Deno.writeTextFile("u.js",x.trim())`,
 ]
-import { UPNG } from "upng-js"
-import * as px from "pxxl"
 import * as base64 from "byte-base64";
+import { fpng } from "fpng"
 //let px = await import("./pxxl.min.js")
 //import "./pako.min.js"
 //import { UPNG } from "./upng.min.js"
@@ -45,125 +44,8 @@ const cur_hash=arr_to_hex(new Uint8Array(await crypto.subtle.digest("SHA-512", n
 
 if (last_hash.trim() != cur_hash.trim()){
  Deno.writeTextFileSync('data_sha512.txt',cur_hash)
+  const img=fpng(text,domain,tss)
 
-const pf = px.Pxxl.Font.ParseBDF(Deno.readTextFileSync("assets/8x16.bdf"))
-const f_sites = 2
-const f_tracks = 80
-const f_sectors = 18
-const f_bytes = 512
-const bl = [0, 0, 0, 255]
-const tl = [0, 153, 136, 255]
-const cy = [51, 187, 238, 255]
-const or = [238, 119, 51, 255]
-const mg = [238, 51, 119, 255]
-const shift = 496
-const dat = new TextEncoder().encode(text)
-const num_lines = 7
-const width = 5464
-const font_height = 16
-const line_below = 1
-const top_padding = 6
-const bottom_padding = 5
-const head_length = width *
-  (num_lines * (font_height + line_below) + top_padding + bottom_padding)
-console.log(
-  `1.44 MB Floppy Disk Size=${f_sites * f_tracks * f_sectors * f_bytes}`,
-)
-const lwidth = 120
-const factor = .06
-const lstart = lwidth * 272
-let logo = new Uint8Array(lwidth * 4 * lwidth).fill(255)
-let llines = ["     FLOPPY", "      PNG"]
-function fc(c, i, j) {
-  let dc = [128, 128, 128, 255]
-  dc[Math.floor(Math.random() * (3))] = 255
-  let colors = [
-    [255, 255, 255, 255],
-    dc,
-    [0, 153, 136, 255],
-    [187, 187, 187, 255],
-  ]
-  for (let k = 0; k < 4; k++) {
-    logo[i * 4 + lwidth * 4 * j + k] = colors[c][k]
-  }
-}
-for (let i = 0; i < lwidth; i++) {
-  for (let j = 0; j < lwidth; j++) {
-    if (i < 17 * lwidth * factor) fc(2, i, j)
-    if (
-      4 * lwidth * factor < i && i < 13 * lwidth * factor && 0 <= j &&
-      j < 6 * lwidth * factor
-    ) fc(3, i, j)
-    if (
-      3 * lwidth * factor < i && i < 14 * lwidth * factor &&
-      7 * lwidth * factor < j && j < 16 * lwidth * factor
-    ) fc(1, i, j)
-    if (
-      i < 11 * lwidth * factor && i > 9 * lwidth * factor &&
-      j < 5 * lwidth * factor && j > 8
-    ) fc(2, i, j)
-    if (
-      5.2 * lwidth * factor < i && i < 12.2 * lwidth * factor &&
-      9.2 * lwidth * factor < j && j < 13.8 * lwidth * factor
-    ) fc(0, i, j)
-  }
-}
-for (let line in llines) {
-  const offset = line == 0 ? 0 : 16
-  let pixels = pf.getPixels(llines[line])
-  for (let pixel of pixels) {
-    for (let i = 0; i < 4; i++) {
-      logo[
-        offset + lstart - 4 + lwidth * 4 * 16 * line +
-        (pixel.x + pixel.y * lwidth) * 4 + i
-      ] = bl[i]
-    }
-  }
-}
-const head = new Uint8Array(head_length).fill(255)
-const dat_rows = (dat.length) % width == 0
-  ? (dat.length) / width
-  : Math.floor(dat.length / width) + 1
-const dat_fill = new Uint8Array(dat_rows * width - dat.length).fill(
-  32,
-)
-const flp_dat_arr = new Uint8Array([
-  ...head,
-  ...dat,
-  ...dat_fill,
-])
-const start = width * top_padding
-for (let line in lines) {
-  const c = line == 0 ? or : line > 0 && line < 3 ? tl : line == 3 ? mg : cy
-  let pixels = pf.getPixels(lines[line])
-  for (let pixel of pixels) {
-    for (let i = 0; i < 4; i++) {
-      flp_dat_arr[
-        start + shift + line * (font_height + line_below) * width +
-        (pixel.x + pixel.y * width / 4) * 4 + i
-      ] = c[i]
-    }
-  }
-}
-let ptr = 0
-let y = 0
-for (let x = start; x < start + width * 119; x++) {
-  if (x % width == 0) {
-    ptr = 0
-    y++
-  }
-  if (ptr < 120 * 4) flp_dat_arr[x] = logo[y * 120 * 4 + ptr]
-  ptr++
-}
-let img = new Uint8Array(
-  UPNG.encode(
-    [flp_dat_arr],
-    width / 4,
-    dat_rows + top_padding + bottom_padding +
-      (font_height + line_below) * num_lines,
-    0,
-  ),
-)
 let a32h = arr_to_hex(img.slice(-20, -16))
 console.log(`Generated FloppyPNG Size=${img.length}`)
 
@@ -215,12 +97,6 @@ Deno.writeTextFileSync(
   `${domain}.page.html`,
   page
     .replaceAll("thisisimage", `${tss}-${a32h}`)
-    .replaceAll(
-      "thisisdatstart",
-      (top_padding + bottom_padding + (font_height + line_below) * num_lines) *
-        (width + 1),
-    )
-    .replaceAll("unfilteredlinewidth", width + 1)
     .replaceAll("thisisemoji", emoji),
 )
 }
